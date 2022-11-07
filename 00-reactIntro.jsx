d93a5bc0693b321f7d3016b13d0f93cc866c67fd
E. S. Adsiz <98318801+esadsiz@users.noreply.github.com>
2022-11-07 22:54:19 +0300

    Create 00-reactIntro.jsx

---
+++ 00-reactIntro.jsx
@@ -0,0 +1,5 @@
+const selam = <h1> Merhaba Enes! </h1>;
+/* bu DOM'a degisken adinin cagrilmasiyla basilabilir. */
+
+npx create-react-app appismi /* proje olusturma komutu */
+npm install /* projeyi ayaga kaldirir. */
